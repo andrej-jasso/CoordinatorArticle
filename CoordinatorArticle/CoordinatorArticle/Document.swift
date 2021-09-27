eee679e1b015378327b74a776c4f35bf923ab334
andrej.jasso@goodrequest.com <andrejjasso>
2021-09-27 17:47:06 +0200

    feature: Sample project

---
+++ CoordinatorArticle/CoordinatorArticle/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  CoordinatorArticle
+//
+//  Created by Andrej Jasso on 27/09/2021.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
